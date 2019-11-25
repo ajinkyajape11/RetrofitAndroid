36c7ad0216d956dd102f6702b78ef38349b3d1f7
ajinkyajape <ajinkyajape@vetron.in>
2019-11-25 22:42:07 +0530

    Implement API Retrofit Android

--- app/src/main/java/com/retrofitandroid/api/ApiClient.java
+++ app/src/main/java/com/retrofitandroid/api/ApiClient.java
@@ -3,30 +3,22 @@ package com.retrofitandroid.api;
 import android.content.Context;
 
 import com.retrofitandroid.ApiInterface;
-import com.retrofitandroid.mvp.model.MovieDataResponse;
 
 import java.util.HashMap;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
-import retrofit2.Callback;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    Context context;
 
     private Retrofit retrofit = null;
 
     String main_url_local = "https://api.themoviedb.org/3/";
 
-
-    HashMap<String, String> headers;
-
-
     public ApiClient() {
     }
-
 
     public ApiInterface API() {
 
